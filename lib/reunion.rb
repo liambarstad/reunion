8058e28e968a9bb0a8ab106c2bc72e809ff21bb8
liambarstad <liambarstad@gmail.com>
2017-07-26 09:48:20 -0600

    iteration 3, not tested

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -11,4 +11,32 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    total = 0
+    @activities.each do |activity|
+      total += activity.total_cost
+    end
+  end
+
+  def breakout
+    results = {}
+    @activities.each do |activity|
+      activity.owed.keys.each do |participant|
+        if results.include?(participant)
+          results[participant] += activity.participants[participant]
+        else
+          results.store(participant, activity.participants[participant])
+        end
+      end
+    end
+    results
+  end
+
+  def print_summary
+    owed = breakout
+    owed.keys.each do |participant|
+      puts "#{participant}: #{owed[participant]}"
+    end
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -27,4 +27,61 @@ class ReunionTest < Minitest::Test
     assert_instance_of Activity, reunion.activities[0]
     assert_equal 1, reunion.activities.length
   end
+
+  def test_total_cost
+    reunion = Reunion.new("Denver")
+    activity = Activity.new("Brunch")
+    activity.add_participant("Jim", 20)
+    activity.add_participant("Joe", 40)
+    reunion.add_activity(activity)
+
+    assert_equal 60, reunion.total_cost
+  end
+
+  def test_total_cost_multiple
+    reunion = Reunion.new("Denver")
+    activity1 = Activity.new("Brunch")
+    activity1.add_participant("Jim", 20)
+    activity1.add_participant("Joe", 40)
+    reunion.add_activity(activity1)
+    activity2 = Activity.new("Cards")
+    activity2.add_participant("Sally", 60)
+    activity2.add_participant("Jim", 10)
+    reunion.add_activity(activity2)
+
+    assert_equal 130, reunion.total_cost
+  end
+
+  def test_breakout
+    reunion = Reunion.new("Denver")
+    activity1 = Activity.new("Brunch")
+    activity1.add_participant("Jim", 20)
+    activity1.add_participant("Joe", 40)
+    reunion.add_activity(activity1)
+    activity2 = Activity.new("Cards")
+    activity2.add_participant("Sally", 60)
+    activity2.add_participant("Jim", 10)
+    reunion.add_activity(activity2)
+
+    people = reunion.breakout.keys.length
+
+    assert_equal 3, people
+    assert_equal 35, reunion.breakout["Jim"]
+    assert_equal -10, reunion.breakout["Joe"]
+    assert_equal -25, reunion.breakout["Sally"]
+  end
+
+  def test_print_summary
+    reunion = Reunion.new("Denver")
+    activity1 = Activity.new("Brunch")
+    activity1.add_participant("Jim", 20)
+    activity1.add_participant("Joe", 40)
+    reunion.add_activity(activity1)
+    activity2 = Activity.new("Cards")
+    activity2.add_participant("Sally", 60)
+    activity2.add_participant("Jim", 10)
+    reunion.add_activity(activity2)
+
+    assert_equal "Jim: 35\nJoe: -10\nSally: -25", reunion.print_summary
+  end
 end
