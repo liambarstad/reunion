545908fdf01ab8e63e1d0c2efff6343fb40ae3cb
liambarstad <liambarstad@gmail.com>
2017-07-26 09:15:19 -0600

    version 1

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,15 @@
 class Activity
+  attr_reader :name,
+              :participants,
+              :total_cost
+  def initialize(name)
+    @name = name
+    @participants = {}
+    @total_cost = 0
+  end
+
+  def add_participant(name, dollars)
+    @participants.store(name, dollars)
+    @total_cost += dollars
+  end
 end
