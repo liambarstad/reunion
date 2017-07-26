72c514b3d05c7d0fe065e7144bb4637f0c46a33a
liambarstad <liambarstad@gmail.com>
2017-07-26 09:31:49 -0600

    iteration 3 working code

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,14 @@
+require './lib/activity'
+
 class Reunion
+  attr_reader :name,
+              :activities
+  def initialize(name)
+    @name = name
+    @activities = []
+  end
+
+  def add_activity(activity)
+    @activities << activity
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,27 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+  def test_it_has_name
+    reunion = Reunion.new("Denver")
+
+    assert_equal "Denver", reunion.name
+  end
+
+  def test_it_initializes_empty_array
+    reunion = Reunion.new("Denver")
+
+    assert_instance_of Array, reunion.activities
+    assert reunion.activities.empty?
+  end
+
+  def test_add_activity
+    reunion = Reunion.new("Denver")
+    activity = Activity.new("Brunch")
+    activity.add_participant("Jim", 20)
+    activity.add_participant("Joe", 40)
+    reunion.add_activity(activity)
+
+    assert_instance_of Activity, reunion.activities[0]
+    assert_equal 1, reunion.activities.length
+  end
 end
